3b27c7e8050999f8a67fcbc179cc6af888665838
hammartap <hammartap@gmail.com>
2014-03-18 03:03:21 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,41 @@
+# facet-liveview
+
+An example application which demonstrates liveview retrieving of Sony Remote Camera Control API Beta.
+
+This example uses [facet](https://github.com/hammartap/facet) for retrieve liveview from Sony camera, and [Quil](https://github.com/quil/quil) for visualisation.
+
+## Usage
+1. Make sure your machine is connected Sony Camera via WiFi.
+2. Clone this repository, then fire up REPL.
+3. Import the library and execute `(-main)`
+
+```
+$ git clone https://github.com/hammartap/facet-liveview.git
+$ cd facet-liveview
+$ lein repl
+```
+
+```clojure
+;; Change namespace.
+;; This is optional. But it is helpful when playing with this library.
+(ns facet-liveview.core)
+
+;; Import a library.
+(use 'facet-liveview.core)
+
+;; Then, call main function.
+(-main)
+
+;; Take a picture.
+;; This will returns a map which contains URL of a photo and API executed id.
+(takeAndFetchPicture)
+
+;; Finish the window.
+(ql/sketch-close liveview)
+```
+
+## License
+
+Copyright © 2014 hammartap
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ src/facet_photo/core.clj
@@ -0,0 +1,61 @@
+(ns facet-photo.core
+  (:require [quil.core :as ql])
+  (:require [facet.core :as ft])
+
+  (:import javax.imageio.ImageIO)
+  (:import java.io.ByteArrayInputStream)
+
+  (:import [processing.core PConstants])
+  (:gen-class))
+
+
+;; An atom which holds data of a taken photo.
+(def ^:private PhotoData (atom nil))
+
+
+;; You can take a picture at any time after calling -main function.
+(defn shutter!
+  "Take a picture."
+  []
+  (reset! PhotoData (ft/takeAndFetchPicture)))
+
+
+;; ## Setup function for Quil
+(defn- setup []
+  (ql/smooth)
+  (ql/frame-rate 30)
+  
+  ;; Set the background colour to a nice shade of grey.
+  (ql/background 200)
+  
+  ;; Initialize the camera.
+  ;; And start retrieving liveview images.
+  (ft/init)
+  ;; Wai a bit.
+  (Thread/sleep 1000)
+  (shutter!))
+
+
+;; ## Draw function for Quil
+(defn- draw []
+  (let [url (:result @PhotoData)
+        pimg   (when-not (= url nil)
+                 (ql/load-image url))
+        ;;width  (.width pimg)
+        ;;height (.height pimg)
+        ;;pixels (.pixels pimg)
+        ]
+    ;; Display pimg
+    (ql/image pimg 0 0)))
+
+
+;; ## Sketch defenition for Quil
+(defn -main
+  "This is liveview example."
+  [& args]
+  (ql/defsketch photo                    ;; Define a new sketch named liveview
+    :title "Sony Camera Photo Taking Sample." ;; Set the title of the sketch
+    :setup setup                         ;; Specify the setup fn
+    :draw draw                           ;; Specify the draw fn
+    :size [1440 1080])                     ;; You struggle to beat the golden ratio
+  )
